c7f9c8826e9c1caefdc9851a926d40bcbe4fc4d1
yury <kentyku@bk.ru>
2020-06-10 02:11:45 +0400

    Create empty app

---
+++ src/main/java/ru/kentyku/converterformockserver/ConverterformockServerApplication.java
@@ -0,0 +1,13 @@
+package ru.kentyku.converterformockserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConverterformockServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConverterformockServerApplication.class, args);
+	}
+
+}
